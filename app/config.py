65c67eca34f79680272976946fff1fe47d37096d
fnitiwat <fnitiwat001@gmail.com>
2023-05-13 17:20:29 +0700

    remove comment

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 
 class Config(BaseSettings):
     service_account_path: str = "../credentials/service_account.json"
-    to_list_file_mime_types: List[str] = ["text/plain", "text/x-sql", "text/markdown", "text/csv"] + ["application/vnd.google-apps.folder"] # Note: not supported yet because this filter will exclude folder too
+    to_list_file_mime_types: List[str] = ["text/plain", "text/x-sql", "text/markdown", "text/csv"] + ["application/vnd.google-apps.folder"]
     to_read_content_mime_types: List[str] = ["text/plain", "text/x-sql", "text/markdown", "text/csv"]
     to_read_content_size_threshold: int = 100000 # byte
 
